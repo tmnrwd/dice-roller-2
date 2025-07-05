8d940e843755e4064c2457f0a8c610c0c4fa6b42
Tim Norwood <tmvnorwood@gmail.com>
2025-07-05 04:05:01 +0100

    add modifier parameter

--- src/app/js/diceLogic.ts
+++ src/app/js/diceLogic.ts
@@ -7,11 +7,12 @@ export function rollDie(sides: number): number {
     return result;
 }
 
-export function rollDice(quantity:number,sides:number): number{
+export function rollDice(quantity:number,sides:number,modifier:number=0): number{
     let result = 0;
     for (let i = 0; i < quantity; i++) {
         result += rollDie(sides);
     } 
+    result += modifier;
     console.log("you rolled " + result);
     return result;
 }
